2abd0ba8241196c0363408a198c2d1491cee4bdf
erin-burns <eyeheartmath@gmail.com>
2017-12-28 16:43:53 -0600

    Updated app to reflect productMatrix new module name

--- app/js/app.js
+++ app/js/app.js
@@ -1,4 +1,4 @@
 'use strict';
 
 var four51 = {};
-four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter','OrderCloud-SpecForms', 'OrderCloud-ProductLightbox', 'OrderCloud-SameAsShippingCheckbox','OrderCloud-ProductMatrix','OrderCloud-Carousel','PremierPrint-ProductZoom','PremierPrint-CustomTextFieldPlaceholder']);
+four51.app = angular.module('451order', ['ngResource', 'ngRoute', 'ngAnimate', 'ngSanitize', 'ngCookies', 'ngTouch', 'ui.validate', 'ui.mask', 'headroom', 'ui.bootstrap', 'angulartics', 'angulartics.google.analytics', 'ngAutocomplete', 'OrderCloud-AnonRouter', 'OrderCloud-SpecForms', 'OrderCloud-ProductLightbox', 'OrderCloud-SameAsShippingCheckbox', 'OrderCloud-Carousel', 'PremierPrint-ProductMatrix', 'PremierPrint-ProductZoom', 'PremierPrint-CustomTextFieldPlaceholder']);
